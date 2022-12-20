c63ee825d16f7db2239fcc592f41bd52412fbe60
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-12-20 13:53:11 +0100

    chore: show commands logs in bash

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -8,12 +8,12 @@ labels_path="$3"
 
 while IFS= read -r tag; do
   tag_with_image="$(echo "${tag}" | sed "s/placeholder_app_name/${app}/g")"
-  tags_params="${tags_params} --tag ${tag_with_image}"
+  tags_params="${tags_params} --tag=\"${tag_with_image}\""
 done < "$tags_path"
 tags_params="$(echo "${tags_params}" | xargs)"
 
 while IFS= read -r label; do
-  labels_params="${labels_params} --label ${label}"
+  labels_params="${labels_params} --label=\"${label}\""
 done < "$labels_path"
 labels_params="$(echo "${labels_params}" | xargs)"
 
