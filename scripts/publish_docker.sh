3f91e42b7a176dd011d4a41bbe49c9f474af490f
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-12-20 14:53:59 +0100

    ci: use plain bash arrays

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -9,12 +9,12 @@ labels_path="$3"
 tags_params=()
 while IFS= read -r tag; do
   tag_with_image="$(echo "${tag}" | sed "s/placeholder_app_name/${app}/g")"
-  tags_params+=("--tag", "${tag_with_image}")
+  tags_params+=("--tag" "${tag_with_image}")
 done < "$tags_path"
 
 labels_params=()
 while IFS= read -r label; do
-  labels_params+=("--label", "${label}")
+  labels_params+=("--label" "${label}")
 done < "$labels_path"
 
 docker buildx build \
