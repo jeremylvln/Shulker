63999f26b60209656f4009ad5c68ebd2bd7beb0d
Jérémy Levilain <jeremy@jeremylvln.fr>
2023-01-02 17:18:25 +0100

    docs: add intro and architecture

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -77,10 +77,6 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Understanding the Basics',
-                to: '/understanding-the-basics/installation',
-              },
-              {
                 label: 'Getting Started',
                 to: '/getting-started/installation',
               },
