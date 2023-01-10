d25d5aa5ed3cc5f7fe4d5884472fe53a2f575d9f
Jérémy Levilain <jeremy@jeremylvln.fr>
2023-01-10 16:01:02 +0100

    refactor: kube resources

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -5,7 +5,7 @@ set -xe
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 ROOTDIR="$(realpath "${SCRIPTPATH}/..")"
 
-# controller-gen rbac:roleName=shulker-operator-role crd webhook paths="${ROOTDIR}/..." output:crd:artifacts:config=${ROOTDIR}/config/crd/bases
-controller-gen crd webhook paths="${ROOTDIR}/..." output:crd:artifacts:config=${ROOTDIR}/config/crd/bases
+# controller-gen rbac:roleName=shulker-operator-role crd webhook paths="${ROOTDIR}/..." output:crd:artifacts:config=${ROOTDIR}/kube/resources/crd/bases
+controller-gen crd webhook paths="${ROOTDIR}/..." output:crd:artifacts:config=${ROOTDIR}/kube/resources/crd/bases
 
 controller-gen object:headerFile="${SCRIPTPATH}/hack/boilerplate.go.txt" paths="${ROOTDIR}/..."
