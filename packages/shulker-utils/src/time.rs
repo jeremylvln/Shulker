2f7ec6b47c1d2e6230580d3c9bdad111368ba52b
Jérémy Levilain <jeremy@jeremylvln.fr>
2023-10-13 11:02:01 +0200

    test(shulker-utils): write tests for time module

--- packages/shulker-utils/src/time.rs
+++ packages/shulker-utils/src/time.rs
@@ -20,3 +20,39 @@ pub fn now() -> DateTime<Utc> {
 pub fn set_test_time_seconds(seconds: i64) {
     UTC_TIME_SECONDS.with(|x| x.store(seconds, std::sync::atomic::Ordering::SeqCst));
 }
+
+#[cfg(not(feature = "testing"))]
+mod tests {
+    #[test]
+    fn now_should_return_current_time() {
+        // W
+        let time = super::now();
+
+        // T
+        assert!(time.timestamp() > 0);
+    }
+}
+
+#[cfg(feature = "testing")]
+mod tests {
+    #[test]
+    fn now_should_return_default_time() {
+        // W
+        let time = super::now();
+
+        // T
+        assert_eq!(time.timestamp(), 0);
+    }
+
+    #[test]
+    fn now_should_return_defined_time() {
+        // G
+        super::set_test_time_seconds(42);
+
+        // W
+        let time = super::now();
+
+        // T
+        assert_eq!(time.timestamp(), 42);
+    }
+}
