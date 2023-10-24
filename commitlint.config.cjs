c8c3241f69393138f163d0c849ec9d50c771303b
Jérémy Levilain <jeremy@jeremylvln.fr>
2023-10-24 14:18:57 +0200

    chore: replace commitlint Nx plugin with devkit directly

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,14 +1,19 @@
 const {
-  utils: { getProjects },
-} = require('@commitlint/config-nx-scopes');
+  buildProjectGraphWithoutDaemon,
+} = require('nx/src/project-graph/project-graph');
 
 const EXTRA_SCOPES = ['deps'];
+
+const listNxProjects = async () => {
+  const graph = await buildProjectGraphWithoutDaemon();
+  return Object.values(graph.nodes).map((project) => project.name);
+};
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': (ctx) =>
-      getProjects(ctx).then((packages) => [
+    'scope-enum': () =>
+      listNxProjects().then((packages) => [
         2,
         'always',
         [...packages, ...EXTRA_SCOPES],
