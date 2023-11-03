68f443d7d47a7157739b88a2f8d7a7a492ea6a73
Jérémy Levilain <jeremy@jeremylvln.fr>
2023-11-03 15:23:44 +0100

    feat(google-open-match-sdk): create auto-generated sdk

--- scripts/upload_codecov_files.sh
+++ scripts/upload_codecov_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CODECOV="${CODECOV:-codecov}"
+$CODECOV -t $CODECOV_TOKEN -F google-open-match-sdk-bindings-rust -f ./coverage/rust/google-open-match-sdk-bindings-rust/cobertura.xml
+$CODECOV -t $CODECOV_TOKEN -F shulker-addon-matchmaking -f ./coverage/rust/shulker-addon-matchmaking/cobertura.xml
 $CODECOV -t $CODECOV_TOKEN -F shulker-sdk-bindings-rust -f ./coverage/rust/shulker-sdk-bindings-rust/cobertura.xml
 $CODECOV -t $CODECOV_TOKEN -F google-agones-crds -f ./coverage/rust/google-agones-crds/cobertura.xml
 $CODECOV -t $CODECOV_TOKEN -F shulker-kube-utils -f ./coverage/rust/shulker-kube-utils/cobertura.xml
