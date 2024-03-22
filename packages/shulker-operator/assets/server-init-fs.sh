d92a3258e40bbf0657574244436baee22c84779e
Jérémy Levilain <jeremy@jeremylvln.fr>
2024-03-22 16:54:40 +0100

    feat(shulker-operator): add a magic env var to skip any download in init-fs

--- packages/shulker-operator/assets/server-init-fs.sh
+++ packages/shulker-operator/assets/server-init-fs.sh
@@ -10,19 +10,21 @@ if [ "${SHULKER_VERSION_CHANNEL}" == "Paper" ] || [ "${SHULKER_VERSION_CHANNEL}"
   cp "${SHULKER_CONFIG_DIR}/paper-global-config.yml" "${SHULKER_SERVER_CONFIG_DIR}/config/paper-global.yml"
 fi
 
-if [ ! -z "${SERVER_WORLD_URL+x}" ]; then
-  (cd "${SHULKER_SERVER_CONFIG_DIR}" && wget "${SERVER_WORLD_URL}" -O - | tar -xzv)
-fi
+if [ -z "${SHULKER_SKIP_ANY_DOWNLOAD}" ]; then
+  if [ ! -z "${SERVER_WORLD_URL}" ]; then
+    (cd "${SHULKER_SERVER_CONFIG_DIR}" && wget "${SERVER_WORLD_URL}" -O - | tar -xzv)
+  fi
 
-if [ ! -z "${SHULKER_SERVER_PLUGIN_URLS+x}" ]; then
-  mkdir -p "${SHULKER_SERVER_CONFIG_DIR}/plugins"
-  for plugin_url in ${SHULKER_SERVER_PLUGIN_URLS//;/ }; do
-    (cd "${SHULKER_SERVER_CONFIG_DIR}/plugins" && wget "${plugin_url}")
-  done
-fi
+  if [ ! -z "${SHULKER_SERVER_PLUGIN_URLS}" ]; then
+    mkdir -p "${SHULKER_SERVER_CONFIG_DIR}/plugins"
+    for plugin_url in ${SHULKER_SERVER_PLUGIN_URLS//;/ }; do
+      (cd "${SHULKER_SERVER_CONFIG_DIR}/plugins" && wget "${plugin_url}")
+    done
+  fi
 
-if [ ! -z "${SHULKER_SERVER_PATCH_URLS+x}" ]; then
-  for patch_url in ${SHULKER_SERVER_PATCH_URLS//;/ }; do
-    (cd "${SHULKER_SERVER_CONFIG_DIR}" && wget "${patch_url}" -O - | tar -xzv)
-  done
+  if [ ! -z "${SHULKER_SERVER_PATCH_URLS}" ]; then
+    for patch_url in ${SHULKER_SERVER_PATCH_URLS//;/ }; do
+      (cd "${SHULKER_SERVER_CONFIG_DIR}" && wget "${patch_url}" -O - | tar -xzv)
+    done
+  fi
 fi
