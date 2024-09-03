68acf16949967d4def24306124e1d1b5f548cdcd
Hugo Planque <hookwood01@gmail.com>
2024-09-03 03:46:57 +0200

    feat: support the override of agones health check in proxy and minecraft template
    feat: add health docs to podOverride

--- packages/google-agones-crds/src/v1/game_server.rs
+++ packages/google-agones-crds/src/v1/game_server.rs
@@ -33,12 +33,19 @@ pub struct GameServerPortSpec {
 #[derive(Deserialize, Serialize, Clone, Debug, JsonSchema, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct GameServerHealthSpec {
+    /// Specify whether the health check should be disabled.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub disabled: Option<bool>,
+
+    /// Specify the period in seconds between health checks.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub period_seconds: Option<i32>,
+
+    /// Specify how much health failure is tolerated before the GameServer is considered unhealthy.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub failure_threshold: Option<i32>,
+
+    /// Specify the initial delay in seconds before the first health check is performed.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub initial_delay_seconds: Option<i32>,
 }
